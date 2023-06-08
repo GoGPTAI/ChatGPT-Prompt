b5d7682c8db0fdd97837de92594e3881ca24382e
GoGPT <135786074+GoGPTAI@users.noreply.github.com>
2023-06-08 09:31:24 +0800

    Update go.js

--- go.js
+++ go.js
@@ -1,1 +1,12 @@
 
+ChatGPT Prompts
+Welcome to the "ChatGPT Prompts" repository! This is a collection of prompt examples to be used with the ChatGPT model.
+
+The ChatGPT model is a large language model trained by OpenAI that is capable of generating human-like text. By providing it with a prompt, it can generate responses that continue the conversation or expand on the given prompt.
+
+In this repository, you will find a variety of prompts that can be used with ChatGPT. We encourage you to add your own prompts to the list, and to use ChatGPT to generate new prompts as well.
+
+To get started, simply clone this repository and use the prompts in the README.md file as input for ChatGPT. You can also use the prompts in this file as inspiration for creating your own.
+
+We hope you find these prompts useful and have fun using ChatGPT!
+
